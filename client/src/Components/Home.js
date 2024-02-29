62bdc070a2f824d86ac98806a5d2164a01aeedbc
Apekshakini <apekshakini.n@gmail.com>
2024-02-29 12:49:16 +0530

    done

---
+++ client/src/Components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <>
+      <div style={{ textAlign: "center" }}>
+        <h1>Welcome to Our Food App!</h1>
+        <p>Explore our delicious selection of food items.</p>
+        <img src="/images/food.jpeg" alt="Food App" style={{ maxWidth: "100%", height: "auto" }} />
+      </div>
+    </>
+  );
+};
+
+export default Home;
